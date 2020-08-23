777fa62bbd527dd8f4e0947293d26859b507cac0
MRPTY1 <ymworld1102@gmail.com>
2020-08-23 21:25:25 +0800

    添加system使用

---
+++ Test/Test/hellow.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+//std standard标准 io 输入输出
+int main01() {
+	printf("Hellow World");
+	return 0;
+} 
---
+++ Test/Test/system.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+//lib 标准静态库
+#include<stdio.h>
+int main02() {
+	int value;
+	//运行cmd命令 打开写字板
+	//返回值为0和其他数
+	value=system("write");
+	printf("%d\n", value);
+	return 0;
+}
